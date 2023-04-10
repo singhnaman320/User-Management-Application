a2464c380e369b1c08cfb3cfde5fb7cd76e3cfaa
naman kumar singh <singhnaman320@gmail.com>
2023-04-10 13:24:59 +0530

    Home Page FrontEnd Part

---
+++ User_Management_Application/src/main/java/com/management/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package com.management.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		
+		return "register";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "login";
+	}
+	
+	@GetMapping("/info")
+	public String info() {
+		
+		return "info";
+	}
+}
