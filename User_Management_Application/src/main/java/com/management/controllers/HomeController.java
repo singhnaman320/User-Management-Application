cefed696d1f6f9e9b95a8835fd11be5a19c82ea6
naman kumar singh <singhnaman320@gmail.com>
2023-04-10 20:55:33 +0530

    Modification in Controller and FrontEnd Part

--- User_Management_Application/src/main/java/com/management/controllers/HomeController.java
+++ User_Management_Application/src/main/java/com/management/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String index() {
 		
 		return "index";
