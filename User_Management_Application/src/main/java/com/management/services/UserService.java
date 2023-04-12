ed644b6d7528bbbcd9c0adb89b005ed38be451b7
naman kumar singh <singhnaman320@gmail.com>
2023-04-12 10:38:38 +0530

    Adding service and its implementation

---
+++ User_Management_Application/src/main/java/com/management/services/UserService.java
@@ -0,0 +1,9 @@
+package com.management.services;
+
+import com.management.entities.User;
+import com.management.exceptions.UserNotFoundException;
+
+public interface UserService {
+
+	public User createUser(User user)throws UserNotFoundException;
+}
---
+++ User_Management_Application/src/main/java/com/management/servicesImplementations/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.management.servicesImplementations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.management.entities.User;
+import com.management.exceptions.UserNotFoundException;
+import com.management.repositories.UserRepository;
+import com.management.services.UserService;
+
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public User createUser(User user) throws UserNotFoundException {
+		// TODO Auto-generated method stub
+		
+		if(user != null) {
+			
+			User saveUser= userRepository.save(user);
+			return saveUser;
+		}
+		else 
+			
+			throw new UserNotFoundException("Facing problem in creating new user");
+	}
+
+}
