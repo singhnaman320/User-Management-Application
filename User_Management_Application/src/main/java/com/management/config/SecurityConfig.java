4a01785697bd0a0fd2590b11c1ea4dd6ee6913bd
naman kumar singh <singhnaman320@gmail.com>
2023-04-13 15:10:04 +0530

    Adding Security config, Modifying pom.xml

--- User_Management_Application/src/main/java/com/management/config/SecurityConfig.java
+++ User_Management_Application/src/main/java/com/management/config/SecurityConfig.java
@@ -1,5 +1,31 @@
 package com.management.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests()
+		.antMatchers("/admin/**")
+		.hasRole("ADMIN")
+		.antMatchers("/user/**")
+		.hasRole("USER")
+		.antMatchers("/**")
+		.permitAll()
+		.and()
+		.formLogin()
+		.loginPage("/signIn")
+		.loginProcessingUrl("/login")
+		.defaultSuccessUrl("/user/")
+		.and()
+		.csrf().disable();
+	}
 
 }
